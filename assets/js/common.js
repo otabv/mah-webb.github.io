4851a1a3baef51b5cc394c4d0a8e8129b53ee1c0
Sebastian Bengtegård <sebbebook@gmail.com>
2014-08-22 11:18:59 +0200

    continued L03, added js/css for calculator

--- assets/js/common.js
+++ assets/js/common.js
@@ -44,13 +44,52 @@
         }
     }
 
+    var calculator = document.getElementById( 'grid-calculator' );
+
+    if ( calculator ) {
+        var columns = document.getElementById( 'grid-columns' ),
+            margin = document.getElementById( 'grid-margin' ),
+            padding = document.getElementById( 'grid-padding' ),
+            border = document.getElementById( 'grid-border' );
+
+        var result = document.getElementById( 'grid-result' );
+
+        console.log( columns, margin, padding, border, result );
+
+        function calculateWidth() {
+            var m = margin.value || 0,
+                p = padding.value || 0,
+                b = border.value || 0;
+
+            var w = 80 * columns.value - ( ( m * 2 ) + ( p * 2 ) + ( b * 2 ) );
+
+            result.innerHTML = +w;
+        }
+
+        [columns, margin, padding, border].forEach( function( input ) {
+            input.addEventListener( 'change', function() {
+                calculateWidth();
+            }, false );
+        });
+
+        [margin, padding, border].forEach( function( input ) {
+            input.addEventListener( 'keyup', function() {
+                calculateWidth();
+            }, false );
+        })
+    }
+
     // Simple solution for a sticky footer
-    window.onload = function() {
+    function fixedFooter() {
         var h = window.innerHeight - body.clientHeight;
 
         if ( h > 0 ) {
             var c = document.querySelector( '.content' );
             c.style.minHeight = ( 300 + h ) + "px";
         }
+    }
+
+    window.onload = function() {
+        fixedFooter();
     };
 })()
