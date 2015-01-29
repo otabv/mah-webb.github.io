2f69613d5f5b7f6f2f0ef617e955708c6b0a7417
bopeterson <bo.peterson@gmail.com>
2015-01-29 14:29:41 +0100

    lecture 3

---
+++ courses/me132a/lectures/resources3/formoutput.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Form output</title>
+</head>
+
+<body>
+åäö
+<?php
+
+$firstname=$_GET['firstname'];
+$lastname=$_GET['lastname'];
+
+echo "Välkommen $firstname $lastname";
+
+?>
+
+</body>
+</html>
